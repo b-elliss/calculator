117d602e22dba378f6a4663e83b73ed8328af128
Brandon Ellis <ellis.brandon.w@gmail.com>
2023-11-15 22:38:50 -0500

    multiple operation strings bug fixed

--- script.js
+++ script.js
@@ -1,38 +1,45 @@
 let tempDispValue = '';
-let firstNum = []; //assign this value whenever the operator is clicked
-let operator = []; //assign this value whenever the operator is clicked
-let secondNum = [];//assign this value whenever the equal sign is clicked
+let firstNum; 
+let operator; 
+let secondNum;
+let display = document.querySelector('.display');
+
 const numberButtons = document.querySelectorAll('#number-button');
 const operatorButtons = document.querySelectorAll('#operator-button');
 const equalButton = document.querySelector('#equal-button');
 const clearButton = document.querySelector('#clear-button');
 
+
 numberButtons.forEach(button => 
     button.addEventListener('click', function() {
         tempDispValue = tempDispValue + this.textContent;
-        document.querySelector('.display').textContent = tempDispValue;
+        display.textContent = tempDispValue;
     }));
 
 operatorButtons.forEach(button => 
     button.addEventListener('click', function() {
-        firstNum = tempDispValue;
+        if (firstNum === undefined) {
+            firstNum = tempDispValue
+        } else {
+            secondNum = tempDispValue;
+            firstNum = operate();
+        };
         operator = this.textContent;
         tempDispValue = '';
-        document.querySelector('.display').textContent = tempDispValue;
         }));
 
 equalButton.addEventListener('click', function() {
     secondNum = tempDispValue;
     tempDispValue = operate();
-    document.querySelector('.display').textContent = tempDispValue;
+    display.textContent = tempDispValue;
 });
 
 clearButton.addEventListener('click', function() {
     tempDispValue = '';
-    document.querySelector('.display').textContent = tempDispValue;
-    firstNum = []; 
-    operator = []; 
-    secondNum = [];
+    display.textContent = tempDispValue;
+    firstNum = undefined; 
+    operator = undefined; 
+    secondNum = undefined;
 });
 
 function operate() {

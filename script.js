6e4bf4b3b3104b34eef239fbb4b6e156f8984987
Brandon Ellis <ellis.brandon.w@gmail.com>
2023-11-19 20:01:07 -0500

    changed styles, added delete button, and fixed sum calculation bug

--- script.js
+++ script.js
@@ -1,50 +1,98 @@
-let tempDispValue = '';
+let sum;
 let firstNum; 
 let operator; 
 let secondNum;
+let tempValue = '';
 let display = document.querySelector('.display');
-
-const numberButtons = document.querySelectorAll('#number-button');
-const operatorButtons = document.querySelectorAll('#operator-button');
 const equalButton = document.querySelector('#equal-button');
 const clearButton = document.querySelector('#clear-button');
+const deleteButton = document.querySelector('#delete-button');
+const numberButtons = document.querySelectorAll('#number-button');
+const operatorButtons = document.querySelectorAll('#operator-button');
 
 
 numberButtons.forEach(button => 
     button.addEventListener('click', function() {
-        tempDispValue = tempDispValue + this.textContent;
-        display.textContent = tempDispValue;
+        if (display.textContent == 'ERROR' || display.textContent == 'nope') {
+            clear();
+        };
+        if (tempValue.includes('.') == true) {
+            if (this.textContent == '.' == true) {
+                tempValue = tempValue + this.textContent
+                tempValue = tempValue.slice(0, -1);
+            } else {
+                tempValue = tempValue + this.textContent
+            };
+        } else {tempValue = tempValue + this.textContent}
+        display.textContent = tempValue;
     }));
+
+deleteButton.addEventListener('click', function() {
+    if (display.textContent == tempValue) {
+        tempValue = tempValue.slice(0, -1);
+        display.textContent = tempValue
+    };
+    if (display.textContent == sum) {
+        sum = +sum.toString().slice(0, -1);
+        display.textContent = sum;
+    };
+    if (display.textContent == 'ERROR' || display.textContent == 'nope') {
+        clear();
+    };
+});
 
 operatorButtons.forEach(button => 
     button.addEventListener('click', function() {
-        if (firstNum === undefined) {
-            firstNum = tempDispValue
+        if (sum !== undefined) {
+            firstNum = sum;
+        } else if (firstNum === undefined) {
+            firstNum = tempValue;
         } else {
-            secondNum = tempDispValue;
+            secondNum = tempValue;
+            tempValue = '';
             if (operator == '/' && secondNum == 0) {
-                tempDispValue = 'nope';
-                return display.textContent = tempDispValue;}
-            else {firstNum = Math.round((operate() + Number.EPSILON) * 100) / 100};
+                return display.textContent = 'nope';
+            } else {
+                sum = Math.round((operate() + Number.EPSILON) * 100) / 100;
+                firstNum = sum;
+                secondNum = undefined;
+            }
         };
         operator = this.textContent;
-        tempDispValue = '';
+        tempValue = '';
         }));
 
 equalButton.addEventListener('click', function() {
-    secondNum = tempDispValue;
-    if (operator == '/' && secondNum == 0) {tempDispValue = 'nope'}
-    else {tempDispValue = Math.round((operate() + Number.EPSILON) * 100) / 100};
-    display.textContent = tempDispValue;
+    secondNum = tempValue;
+    tempValue = '';
+    if (firstNum == undefined || secondNum == '') {
+        return display.textContent = 'ERROR';
+    };
+    if (operator == '/' && secondNum == 0) {
+        return display.textContent = 'nope';
+    } else {
+        sum = Math.round((operate() + Number.EPSILON) * 100) / 100;
+        secondNum = undefined;
+    };
+    display.textContent = sum;
+    console.log(tempValue);
+    console.log(firstNum);
+    console.log(operator);
+    console.log(secondNum);
+    console.log(sum)
+
 });
 
-clearButton.addEventListener('click', function() {
-    tempDispValue = '';
-    display.textContent = tempDispValue;
+clearButton.addEventListener('click', clear);
+
+function clear() {
+    tempValue = '';
     firstNum = undefined; 
     operator = undefined; 
     secondNum = undefined;
-});
+    sum = undefined
+    display.textContent = tempValue;
+};
 
 function operate() {
     if (operator == '+') {return +firstNum + +secondNum};

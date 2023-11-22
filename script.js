97a51332552d6c6ecae926cd61b46ee543884b7f
Brandon Ellis <ellis.brandon.w@gmail.com>
2023-11-21 20:23:38 -0500

    keyboard compatibility added

--- script.js
+++ script.js
@@ -10,23 +10,78 @@ const deleteButton = document.querySelector('#delete-button');
 const numberButtons = document.querySelectorAll('#number-button');
 const operatorButtons = document.querySelectorAll('#operator-button');
 
-numberButtons.forEach(button => 
-    button.addEventListener('click', function() {
-        if (display.textContent == 'ERROR' || display.textContent == 'nope') {
-            clear();
-        };
-        if (tempValue.includes('.') == true) {
-            if (this.textContent == '.' == true) {
-                tempValue = tempValue + this.textContent
-                tempValue = tempValue.slice(0, -1);
-            } else {
-                tempValue = tempValue + this.textContent
-            };
-        } else {tempValue = tempValue + this.textContent}
-        display.textContent = tempValue;
-    }));
 
-deleteButton.addEventListener('click', function() {
+numberButtons.forEach(button => 
+    button.addEventListener('click', generalButtonFunc));
+
+operatorButtons.forEach(button => 
+    button.addEventListener('click', operatorButtonFunc));
+
+equalButton.addEventListener('click', equalButtonFunc);
+
+deleteButton.addEventListener('click', deleteButtonFunc);
+
+clearButton.addEventListener('click', clear);
+
+
+function operate() {
+    if (operator == '+') {return +firstNum + +secondNum};
+    if (operator == '-') {return +firstNum - +secondNum};
+    if (operator == 'x' || operator == 'x') {return +firstNum * +secondNum};
+    if (operator == '÷' || operator == '/') {return +firstNum / +secondNum};
+};
+
+function generalButtonFunc() {
+    if (display.textContent == 'ERROR' || display.textContent == 'nope') {
+        clear();
+    };
+    if (tempValue.includes('.') == true) {
+        if (this.textContent == '.' == true) {
+            tempValue = tempValue + this.textContent
+            tempValue = tempValue.slice(0, -1);
+        } else {
+            tempValue = tempValue + this.textContent
+        };
+    } else {tempValue = tempValue + this.textContent}
+    display.textContent = tempValue;
+};
+
+function operatorButtonFunc() {
+    if (sum !== undefined) {
+        firstNum = sum;
+    } else if (firstNum === undefined) {
+        firstNum = tempValue;
+    } else {
+        secondNum = tempValue;
+        tempValue = '';
+        if (operator == '/' && secondNum == 0) {
+            return display.textContent = 'nope';
+        } else {
+            sum = Math.round((operate() + Number.EPSILON) * 100) / 100;
+            firstNum = sum;
+            secondNum = undefined;
+        }
+    };
+    operator = this.textContent;
+    tempValue = '';
+};
+
+function equalButtonFunc() {
+    secondNum = tempValue;
+    tempValue = '';
+    if (firstNum == undefined || secondNum == '') {
+        return display.textContent = 'ERROR';
+    };
+    if (operator == '/' && secondNum == 0) {
+        return display.textContent = 'nope';
+    } else {
+        sum = Math.round((operate() + Number.EPSILON) * 100) / 100;
+        secondNum = undefined;
+    };
+    display.textContent = sum;
+};
+
+function deleteButtonFunc() {
     if (display.textContent == tempValue) {
         tempValue = tempValue.slice(0, -1);
         display.textContent = tempValue
@@ -37,11 +92,51 @@ deleteButton.addEventListener('click', function() {
     };
     if (display.textContent == 'ERROR' || display.textContent == 'nope') {
         clear();
-    };
-});
+    }
+};
 
-operatorButtons.forEach(button => 
-    button.addEventListener('click', function() {
+function clear() {
+    tempValue = '';
+    firstNum = undefined; 
+    operator = undefined; 
+    secondNum = undefined;
+    sum = undefined
+    display.textContent = tempValue;
+};
+
+
+
+//Keyboard functionality
+
+document.addEventListener('keydown',numKeyFunc);
+
+document.addEventListener('keydown',operatorKeyFunc);
+
+document.addEventListener('keydown',enterKeyFunc);
+
+document.addEventListener('keydown',deleteKeyFunc);
+
+document.addEventListener('keydown',escKeyFunc);
+
+function numKeyFunc(e) {
+    if (e.key >= 0 && e.key <= 9 || e.key == '.') {
+        if (display.textContent == 'ERROR' || display.textContent == 'nope') {
+            clear();
+        };
+        if (tempValue.includes('.') == true) {
+            if (this.textContent == '.' == true) {
+                tempValue = tempValue + e.key
+                tempValue = tempValue.slice(0, -1);
+            } else {
+                tempValue = tempValue + e.key
+            };
+        } else {tempValue = tempValue + e.key}
+        display.textContent = tempValue;
+    }
+};
+
+function operatorKeyFunc(e) {
+    if (e.key == '+' || e.key == '-' || e.key == '/' || e.key == '*') {
         if (sum !== undefined) {
             firstNum = sum;
         } else if (firstNum === undefined) {
@@ -57,39 +152,18 @@ operatorButtons.forEach(button =>
                 secondNum = undefined;
             }
         };
-        operator = this.textContent;
+        operator = e.key;
         tempValue = '';
-        }));
-
-equalButton.addEventListener('click', function() {
-    secondNum = tempValue;
-    tempValue = '';
-    if (firstNum == undefined || secondNum == '') {
-        return display.textContent = 'ERROR';
-    };
-    if (operator == '/' && secondNum == 0) {
-        return display.textContent = 'nope';
-    } else {
-        sum = Math.round((operate() + Number.EPSILON) * 100) / 100;
-        secondNum = undefined;
-    };
-    display.textContent = sum;
-});
-
-clearButton.addEventListener('click', clear);
-
-function clear() {
-    tempValue = '';
-    firstNum = undefined; 
-    operator = undefined; 
-    secondNum = undefined;
-    sum = undefined
-    display.textContent = tempValue;
+    }
+};
+function enterKeyFunc(e) {
+    if (e.key == 'Enter') {equalButtonFunc()}
 };
 
-function operate() {
-    if (operator == '+') {return +firstNum + +secondNum};
-    if (operator == '-') {return +firstNum - +secondNum};
-    if (operator == 'x') {return +firstNum * +secondNum};
-    if (operator == '÷') {return +firstNum / +secondNum};
+function deleteKeyFunc(e) {
+    if (e.key == 'Backspace') {deleteButtonFunc()}
+};
+
+function escKeyFunc(e) {
+    if (e.key == 'Escape') {clear()}
 };
